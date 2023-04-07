20367c6809a76553cda3f0b1aa3b25381c1e8c95
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-04-07 09:51:31 +0700

    p

--- EASD01/EASD01.cpp
+++ EASD01/EASD01.cpp
@@ -1,5 +1,5 @@
 // 1. Karena dengan algoritma, kita dapat menyelesaikan suatu masalah dengan cepat, dan efektif dengan langkah-langkah yang sistematis
-// 2. statis (jumlah data/ukurannya diketahui atau tetap), dinamis (jumlah data/ukurannya bisa diubah, dihapus, atau ditambah)
+// 2. statis (jumlah data/ukurannya diketahui atau tetap), dinamis (jumlah data /ukurannya bisa diubah, dihapus, atau ditambah)
 // 3. Compiler (penerjemah), ukuran input, kecepatan mesin, Bahasa pemrograman, penggunaan memori
 // 4. bubble sort dan insertion sort, tapi insertion sort lebih simpel dan lebih cepat dalam mengurutkan data dibanding bubble sort.
 // 5. - Quadratic “Bubble sort, selection sort, insertion sort”
